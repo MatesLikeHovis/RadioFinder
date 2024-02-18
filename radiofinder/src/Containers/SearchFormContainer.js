5f984a50aa61f5dcdcfe127edc1ca6b307810fd0
Brian Bunker <bbunk001@ucr.edu>
2024-02-17 20:10:32 -0800

    reached normal functionality

--- radiofinder/src/Containers/SearchFormContainer.js
+++ radiofinder/src/Containers/SearchFormContainer.js
@@ -5,28 +5,89 @@ function SearchFormContainer() {
 
     const [isFetched, setIsFetched] = useState(false);
     const [searchResult, setSearchResult] = useState('');
-    
+    const [address, setAddress] = useState('');
+    const [geoLocation, setGeoLocation] = useState({x: 0.0, y: 0.0});
+    const [station, setStation] = useState('')
+
+    const handleChange = (event) => {
+        setAddress(event.target.value)
+    }
+
+    const searchAddress = async (event) => {
+        async function fetchAddress() {
+            const uriAddress = encodeURIComponent(address);
+            const url = `https://nominatim.openstreetmap.org/search?q=${uriAddress}&format=geojson`
+            try {
+                const response = await fetch(url);
+                if (response.ok) {
+                    const addData = await response.json();
+                    setGeoLocation({x: addData.features[0].geometry.coordinates[0], y: addData.features[0].geometry.coordinates[1]});
+                    console.log(geoLocation);
+                    console.log(addData);
+                } else {
+                    throw new Error('Network response was not good for address search');
+                    console.log(url);
+                }
+            } catch {
+                console.log('There was an unexpected error in finding the geolocation.')
+            }
+        }
+        fetchAddress();
+    }
+
+    const findWeatherStation = async () => {
+        async function fetchStation() {
+            const url = `https://api.weather.gov/points/${geoLocation.y},${geoLocation.x}`
+            try {
+                const response = await fetch(url);
+                if (response.ok) {
+                    const geoData = await response.json();
+                    setStation(geoData.properties.forecast);
+                    console.log(geoData);
+                } else {
+                    throw new Error('Network response was not good for station identification')
+                }
+            } catch {
+                console.log('There was an unexpected error in finding the nearest station.')
+            }
+        }
+        fetchStation();
+    }
+
+    const pullWeather = async () => {
+        const url = station;
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+                setIsFetched(true);
+                setSearchResult(data.properties.periods);
+                console.log(data.properties.periods);
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+    }
+
     useEffect(() => {
+        if (geoLocation.x !== 0 || geoLocation.y !== 0) {
+            findWeatherStation();
+        }
+    }, [geoLocation]);
+
+    useEffect(() => {
+        if (station != '') {
+            pullWeather();
+        }
+    }, [station]);
+
+    const searchForecast = async (event) => {
+        event.preventDefault();
         setIsFetched(false);
-        async function fetchData() {
-          const url = `https://api.weather.gov/gridpoints/TOP/32,81/forecast`;
         try {
-          const response = await fetch(url);
-          if (response.ok) {
-            const data = await response.json();
-            setIsFetched(true);
-            setSearchResult(data.properties.periods)
-            console.log(data.properties.periods);
-          } else {
-            throw new Error('Network response was not ok.');
-            
-          }
-        } catch {
-            console.log('Error');
+            await searchAddress();
+        }   catch {
+            console.log('Error! Please try again.');
         }}
-    
-        fetchData();
-      }, [])
+             
 
     const parsedResult = () => {
         return searchResult.map((item)=>{
@@ -55,8 +116,13 @@ function SearchFormContainer() {
 
     return (
         <>
-            <h1>Hello Search!</h1>
-            <p>Search Results:</p>
+            <h3>Search Address Here:</h3>
+            <form onSubmit = {searchForecast}>
+                <label for="address">Enter Address Here:</label>
+                <input id="address" type="text" value={address} onChange={handleChange}/>
+                <input id="submit" type="submit" />
+            </form>
+            <h4>Search Results:</h4>
             <table>
                 <caption>
                     Weather forecast
@@ -71,5 +137,4 @@ function SearchFormContainer() {
         </>
     )
 }
-
 export default SearchFormContainer;
